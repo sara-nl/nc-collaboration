0d1c33b0f4984693618a9dfb8182eae371dfde93
Antoon Prins <antoon.prins@surf.nl>
2025-02-18 11:04:05 +0100

    Stop each container

--- tests/docker/docker-cleanup.sh
+++ tests/docker/docker-cleanup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-docker compose -f docker-compose-local.yaml stop
+docker container stop nginx-proxy
+docker container stop nc-1
+docker container stop nc-2
+docker container stop mariadb-nc-1
+docker container stop mariadb-nc-2
 docker system prune -f
 docker volume rm -f docker_files-nc-1
 docker volume rm -f docker_mysql-nc-1
